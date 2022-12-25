288952d2d5799ac4018e59182046a622f80f662d
nyiyui <+@nyiyui.ca>
2022-12-25 18:16:39 -0500

    qrystal

--- flake.nix
+++ flake.nix
@@ -1,18 +1,27 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
     home-manager.url = github:nix-community/home-manager;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    qrystal.url = "github:nyiyui/qrystal/lightmusic";
   };
 
-  outputs = { self, nixpkgs, ... }@attrs: let
+  outputs = { self, nixpkgs, qrystal, ... }@attrs: let
     pkgs = import nixpkgs { config.allowUnfree = true; };
   in {
-    nixosConfigurations.kumi = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.kumi = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       specialArgs = attrs;
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        qrystal.nixosModules.${system}.node
+        ({ ... }: {
+          qrystal.services.node = {
+            enable = true;
+          };
+        })
+      ];
     };
     nixosConfigurations.miyo = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
