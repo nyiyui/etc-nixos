39fe2b8986819cb9a46541c98e8b8ec7aadf7563
nyiyui <+@nyiyui.ca>
2023-02-08 23:53:33 -0500

    tmux: detach-on-destroy off

--- nyiyui/tmux.nix
+++ nyiyui/tmux.nix
@@ -7,5 +7,8 @@
     prefix = "C-a";
     shell = "${pkgs.fish}/bin/fish";
     terminal = "screen-256color";
+    extraConfig = ''
+      set-option -g detach-on-destroy off # https://superuser.com/a/1717231
+    '';
   };
 }
