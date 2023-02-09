995149ff5484649071c941caf1eed073d18812a6
nyiyui <+@nyiyui.ca>
2023-02-08 23:48:56 -0500

    update flake etc

--- gensokyo.nix
+++ gensokyo.nix
@@ -5,19 +5,19 @@
   qrystal.services.node = {
     enable = true;
     config.css = [
-      {
-        comment = "gensokyo";
-        endpoint = "gensokyo.mcpt.nyiyui.ca:39252";
-        tls.certPath = builtins.toFile "gensokyo-cert.pem" (builtins.readFile ./gensokyo-cert.pem);
-        networks = [ "hakurei" ];
-        tokenPath = "/etc/nixos/gensokyo-token";
-        azusa.networks.hakurei = {
-          name = config.networking.hostname;
-        };
-      }
+      #{
+      #  comment = "gensokyo";
+      #  endpoint = "gensokyo.mcpt.nyiyui.ca:39252";
+      #  tls.certPath = builtins.toFile "gensokyo-cert.pem" (builtins.readFile ./gensokyo-cert.pem);
+      #  networks = [ "hakurei" ];
+      #  tokenPath = "/etc/nixos/gensokyo-token";
+      #  azusa.networks.hakurei = {
+      #    name = config.networking.hostname;
+      #  };
+      #}
       {
         comment = "kimihenokore";
-        endpoint = "kimihenokore.nyiyui.ca:39252";
+        endpoint = "qrystal.nyiyui.ca:39252";
         tls.certPath = builtins.toFile "kimihenokore-cert.pem" (builtins.readFile ./kimihenokore-cert.pem);
         networks = [ "haruka" ];
         tokenPath = "/etc/nixos/kimihenokore-token";
