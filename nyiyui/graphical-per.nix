0ccfce86b5dca2ff63c85d2d49d86b2d7011618d
nyiyui <+@nyiyui.ca>
2023-02-21 00:23:50 -0500

    update

--- nyiyui/graphical-per.nix
+++ nyiyui/graphical-per.nix
@@ -10,24 +10,26 @@
         };
         "eDP-1".pos = "0 1080";
       };
-      input = {
-        #"1386:221:Wacom_Bamboo_Connect_Pen" = {
-        #  map_to_output = "DP-1";
-        #};
-      };
     };
   })
   (lib.mkIf (config.home.file.hostname.text == "miyo") {
-    wayland.windowManager.sway.config.output = {
-      "HDMI-A-1" = {
-        mode = "3840x2160@30.000Hz";
-        pos = "3840 0";
-        scale = "1.25";
+    wayland.windowManager.sway.config = {
+      output = {
+        "HDMI-A-1" = {
+          mode = "3840x2160@30.000Hz";
+          pos = "3840 0";
+          scale = "1.25";
+        };
+        "HDMI-A-2" = {
+          mode = "3840x2160@60.000Hz";
+          pos = "0 0";
+          scale = "1.5";
+        };
       };
-      "HDMI-A-2" = {
-        mode = "3840x2160@60.000Hz";
-        pos = "0 0";
-        scale = "1.5";
+      input = {
+        "1386:221:Wacom_Bamboo_Connect_Pen" = {
+          map_to_output = "HDMI-A-1";
+        };
       };
     };
   })
