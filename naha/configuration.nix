ee2e45c2d95186872c0c17697e44deb208b48f8a
nyiyui <+@nyiyui.ca>
2023-05-28 17:01:43 -0400

    naha: jack/sound

--- naha/configuration.nix
+++ naha/configuration.nix
@@ -7,6 +7,7 @@
     { }
     nixos-hardware.nixosModules.lenovo-thinkpad-t470s
     ../common.nix
+    ../sound.nix
   ];
 
   networking.hostName = "naha";
@@ -57,23 +58,6 @@
   services.xserver = {
     layout = "us";
     xkbVariant = "";
-  };
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
