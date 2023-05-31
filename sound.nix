9c19e5c30d08b319ab09336bc45fefb60c22d5bc
nyiyui <+@nyiyui.ca>
2023-05-30 20:24:13 -0400

    ahhhhhhh how fix sound

--- sound.nix
+++ sound.nix
@@ -1,15 +1,13 @@
 { pkgs, ... }:
 {
+  sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
-    config.jack = {
-      context.properties.default.clock.allowed-rates = [ 48000 ];
-    };
+    #jack.enable = true;
   };
 
   environment.systemPackages = with pkgs; [ qjackctl jacktrip ];
