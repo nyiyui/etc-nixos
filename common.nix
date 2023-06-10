09a087994cc92daad8b82e8a52dad876f71cdaeb
nyiyui <+@nyiyui.ca>
2023-06-09 20:26:14 -0400

    [untested] dont start docker on boot (active as-needed using socket)

--- common.nix
+++ common.nix
@@ -24,6 +24,8 @@
 
   # Docker
   virtualisation.docker.enable = true;
+  systemd.services.docker.unitConfig.enable = false;
+  # ↑ docker.socket is still active so not much of an issue (see https://superuser.com/a/1731426)
 
   powerManagement.cpuFreqGovernor = "performance";
 
