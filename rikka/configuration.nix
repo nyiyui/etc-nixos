5c4baa21a7268af4560176b301cea1543b6f9564
nyiyui <+@nyiyui.ca>
2023-06-23 21:37:25 -0400

    add rikka

---
+++ rikka/configuration.nix
@@ -0,0 +1,26 @@
+{ modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/virtualisation/google-compute-image.nix")
+    ./autoUpgrade.nix
+    ./doas.nix
+  ];
+
+  networking.hostName = "rikka";
+
+  system.stateVersion = "23.05";
+
+  users.groups.nyiyui = { };
+  users.users.nyiyui = {
+    isNormalUser = true;
+    description = "Ken Shibata";
+    group = "nyiyui";
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [ firefox chromium syncthing git ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  environment.shells = [ pkgs.fish ];
+  nix.settings.auto-optimise-store = true;
+}
