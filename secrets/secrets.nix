a003cd416d1894b32d5e5759573e0a06e3ca59d0
nyiyui <+@nyiyui.ca>
2023-06-25 02:52:35 -0400

    update keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   hinanawi =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGymaXxH/rgi5nqrasYg6dDeu4NHf516LU2sBSHDQfKC root@hananawi";
   rei =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtn+xypcthqDP/g+6TRaohewCLy22/XeyvvRSkkFBVp root@rei.us-east1-b.c.nyiyui.internal";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHSteZOknWCI5z6gEXB7zWrkS8/zOOGObGs9yEIe8wXg root@rei.us-east1-b.c.nyiyui.internal";
 in {
   "kuromiya-key.pem.age".publicKeys = [ rei ];
   "kuromiya-hinanawi.qrystalct.age".publicKeys = [ hinanawi ];
