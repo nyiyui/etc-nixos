496415fafb1dd6c91911ea5f46e3a47db79261ce
nyiyui <+@nyiyui.ca>
2023-06-26 01:37:49 -0400

    kuromiya: DNSCrypt

--- kuromiya.nix
+++ kuromiya.nix
@@ -28,4 +28,20 @@ in {
     group = "qrystal-node";
     mode = "400";
   };
+
+  services.dnsmasq.settings.server = [ "127.0.0.80" ];
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      listen_addresses = [ "127.0.0.80:53" ]; # TODO IPv6
+      require_dnssec = true;
+      sources.public-resolvers = {
+        urls = ["https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md" "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md" "https://ipv6.download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"];
+        cache_file = "public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        refresh_delay = 72;
+        prefix = "";
+      };
+    };
+  };
 }
