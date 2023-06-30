ae187fc6823babbf1645e3fc954ecc05a6424611
nyiyui <+@nyiyui.ca>
2023-06-30 19:37:18 -0400

    naha: larger cursor

--- nyiyui/per.nix
+++ nyiyui/per.nix
@@ -8,10 +8,11 @@
           "${pkgs.chromium}/bin/chromium '--proxy-server=socks5://10.42.0.1:1080' --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'";
       }];
       output = {
-        "eDP-1" = { pos = "0 0"; };
+        "eDP-1" = { pos = "0 1080"; };
         "HDMI-A-2" = {
-          mode = "1920x1080@60.000Hz";
-          pos = "1920 0";
+          mode = "3840x2160@30.000Hz";
+          pos = "0 0";
+          scale = "2";
         };
       };
     };
