a45ac5c787d1a27cb5686f83858af6efdc03a9d3
nyiyui <+@nyiyui.ca>
2023-06-30 04:23:16 -0400

    cirno: bootloader work??

--- cirno/configuration.nix
+++ cirno/configuration.nix
@@ -8,9 +8,19 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  #boot.loader.grub = {
+  #  enable = true;
+  #  device = "/dev/sda";
+  #  version = 2;
+  #  efiSupport = true;
+  #};
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+  };
 
   networking.hostName = "cirno"; # Define your hostname.
 
