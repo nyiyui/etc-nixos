ac130da15e8f3c769f410da3a3f71549c109cffd
nyiyui <+@nyiyui.ca>
2023-08-02 02:13:55 -0400

    kuromiya-check minor changes

--- kotohira/kuromiya-check.nix
+++ kotohira/kuromiya-check.nix
@@ -6,13 +6,19 @@
   services.nginx = {
     enable = true;
     recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
     virtualHosts."kotohira.msb.q.nyiyui.ca" = {
       enableACME = true;
       addSSL = true;
+      forceSSL = true;
       locations."/" = {
         extraConfig = ''
           add_header Content-Type 'text/plain; charset=utf-8';
-          return 200 '黒宮経由で琴平に接続接続出来ました!';
+          if ($server_addr = '10.59.0.0') {
+            return 200 '黒宮経由で琴平に接続出来ました!';
+          }
+          return 200 'ネット経由で琴平に接続出来ました!';
         '';
       };
       locations."/ip" = {
