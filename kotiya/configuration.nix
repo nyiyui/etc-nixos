3389145a12875629239038aa244c494b6fa9bf1f
nyiyui <+@nyiyui.ca>
2023-11-28 15:03:38 -0500

    aiya 2

--- kotiya/configuration.nix
+++ kotiya/configuration.nix
@@ -34,6 +34,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
-  miyamizu.services.target.enable = true;
 }
