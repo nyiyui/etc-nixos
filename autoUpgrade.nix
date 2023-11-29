dbb6d2d1e5428bb2f40be292ea6b14eea62c46bd
nyiyui <+@nyiyui.ca>
2023-11-29 01:03:49 -0500

    autoUpgrade: https support

--- autoUpgrade.nix
+++ autoUpgrade.nix
@@ -1,69 +1,110 @@
 { config, pkgs, lib, ... }: let
   hostName = config.networking.hostName;
   sshKey = "autoupgrade-${hostName}.id_ed25519";
+  cfg = config.autoUpgrade.config;
 in {
-  system.autoUpgrade = {
-    enable = true;
-    rebootWindow.lower = "03:00";
-    rebootWindow.upper = "05:00";
-    randomizedDelaySec = "1d";
-    persistent = true;
-    dates = lib.mkDefault "Fri 02:30";
-    flake = "/etc/nixos";
-    allowReboot = true;
+  options.autoUpgrade.config.daemonUser = with lib; with types; mkOption {
+    type = "str";
+    description = "User to own/maintain the /etc/nixos folder.";
+    default = "youmu";
   };
-  nix.gc = {
-    options = "--delete-older-than 14d";
-    persistent = true;
-    dates = lib.mkDefault "06:00"; # after reboot window
-    automatic = true;
-    randomizedDelaySec = "1h";
-  };
-  users.users.youmu = {
-    isSystemUser = true;
-    description = "auto-upgrade maintainer";
-    group = "youmu";
-  };
-  users.groups.youmu = { };
-  systemd.timers.autoupgrade-pull = {
-    enable = true;
-    description = "trigger pull of /etc/nixos";
-    timerConfig.OnCalendar = lib.mkDefault "Fri 02:00";
-    timerConfig.Persistent = true;
-    # see .github/workflows/flake-upgrade.yml (runs on Fri 00:00)
-    wantedBy = [ "timers.target" ];
-  };
-  systemd.services.autoupgrade-reset-perms = {
-    enable = true;
-    description = "reset perms for /etc/nixos";
-    script = ''
-      set -eu
-      chmod ug+rwX /etc/nixos -R
-      chown nyiyui:youmu /etc/nixos -R
-    '';
-  };
-  systemd.services.autoupgrade-pull = {
-    enable = true;
-    description = "pull /etc/nixos";
-    serviceConfig = {
-      WorkingDirectory = "/etc/nixos";
-      User = "youmu";
-    };
-    wants = [ "autoupgrade-reset-perms.service" ];
-    after = [ "autoupgrade-reset-perms.service" ];
-    script = ''
-      export GIT_SSH_COMMAND='${pkgs.openssh}/bin/ssh -i ${config.age.secrets.${sshKey}.path} -o IdentitiesOnly=yes -o StrictHostKeyChecking=accept-new -o UserKnownHostsFile=/dev/null'
-      ${pkgs.git}/bin/git \
-        -c 'safe.directory=/etc/nixos' \
-        -c 'core.sharedRepository=group' \
-        pull
-    '';
+  options.autoUpgrade.config.authMethod = with lib; with types; mkOption {
+    type = oneOf [
+      (enum [ "ssh" ])
+      (submodule {
+        options = {
+          https = mkOption {
+            type = submodule {
+              options = {
+                username = mkOption {
+                  type = str;
+                  description = "Username to use with HTTPS authentication.";
+                };
+                passwordFile = mkOption {
+                  type = str;
+                  description = "Path to file with password to use with HTTPS authentication.";
+                };
+              };
+            };
+          };
+        };
+      })
+    ];
+    default = "ssh";
+    description = "Which authentication method to use. SSH requires an SSH key; HTTPS requires a username-password pair";
   };
 
-  age.secrets.${sshKey} = {
-    file = ./secrets/autoupgrade-${hostName}.id_ed25519.age;
-    owner = "youmu";
-    group = "youmu";
-    mode = "400";
+  config = {
+    system.autoUpgrade = {
+      enable = true;
+      rebootWindow.lower = "03:00";
+      rebootWindow.upper = "05:00";
+      randomizedDelaySec = "1d";
+      persistent = true;
+      dates = lib.mkDefault "Fri 02:30";
+      flake = "/etc/nixos";
+      allowReboot = true;
+    };
+    nix.gc = {
+      options = "--delete-older-than 14d";
+      persistent = true;
+      dates = lib.mkDefault "06:00"; # after reboot window
+      automatic = true;
+      randomizedDelaySec = "1h";
+    };
+    users.users.youmu = {
+      isSystemUser = true;
+      description = "auto-upgrade maintainer";
+      group = "youmu";
+    };
+    users.groups.youmu = { };
+    systemd.timers.autoupgrade-pull = {
+      enable = true;
+      description = "trigger pull of /etc/nixos";
+      timerConfig.OnCalendar = lib.mkDefault "Fri 02:00";
+      timerConfig.Persistent = true;
+      # see .github/workflows/flake-upgrade.yml (runs on Fri 00:00)
+      wantedBy = [ "timers.target" ];
+    };
+    systemd.services.autoupgrade-reset-perms = {
+      enable = true;
+      description = "reset perms for /etc/nixos";
+      script = ''
+        set -eu
+        chmod g+rwX /etc/nixos -R
+        chgrp youmu /etc/nixos -R
+      '';
+    };
+    systemd.services.autoupgrade-pull = {
+      enable = true;
+      description = "pull /etc/nixos";
+      serviceConfig = {
+        WorkingDirectory = "/etc/nixos";
+        User = "youmu";
+      };
+      wants = [ "autoupgrade-reset-perms.service" ];
+      after = [ "autoupgrade-reset-perms.service" ];
+      script = if cfg.authMethod == "ssh" then ''
+        export GIT_SSH_COMMAND='${pkgs.openssh}/bin/ssh -i ${config.age.secrets.${sshKey}.path} -o IdentitiesOnly=yes -o StrictHostKeyChecking=accept-new -o UserKnownHostsFile=/dev/null'
+        ${pkgs.git}/bin/git \
+          -c 'safe.directory=/etc/nixos' \
+          -c 'core.sharedRepository=group' \
+          pull
+      '' else ''
+        export GIT_TERMINAL_PROMPT=0
+        ${pkgs.git}/bin/git \
+          -c credential.helper='!f() { echo "username=${cfg.authMethod.https.username}" && printf "password=" && cat "${cfg.authMethod.https.passwordFile}"; }; f' \
+          -c 'safe.directory=/etc/nixos' \
+          -c 'core.sharedRepository=group' \
+          pull
+      '';
+    };
+  
+    age.secrets.${sshKey} = lib.mkIf (cfg.authMethod == "ssh") {
+      file = ./secrets/autoupgrade-${hostName}.id_ed25519.age;
+      owner = "youmu";
+      group = "youmu";
+      mode = "400";
+    };
   };
 }
