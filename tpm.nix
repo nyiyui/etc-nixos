90ad840069330fe28aec9459bfad017bfe9ef8f6
nyiyui <+@nyiyui.ca>
2023-12-06 02:26:56 -0500

    tpm fix

--- tpm.nix
+++ tpm.nix
@@ -1,8 +1,8 @@
-{ ... }:
+{ config, ... }:
 {
   # https://nixos.wiki/wiki/TPM
   security.tpm2.enable = true;
   security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
   security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
-  users.users.nyiyui.extraGroups = [ "tss" ];  # tss group has access to TPM devices
+  users.users.nyiyui.extraGroups = [ config.security.tpm2.tssGroup ];  # tss group has access to TPM devices
 }
