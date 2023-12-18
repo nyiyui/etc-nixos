421d35e5c47df5e1a89aa27e8d3189693e520d08
nyiyui <+@nyiyui.ca>
2023-12-17 21:23:32 -0500

    organize wlsunset

--- nyiyui/nyiyui.nix
+++ nyiyui/nyiyui.nix
@@ -15,6 +15,7 @@
     ./rclone.nix
     ./pexec.nix
     ./kicad.nix
+    ./wlsunset.nix
   ];
 
   nixpkgs.config.allowUnfree = true;
@@ -117,15 +118,6 @@
     else
       "JetBrainsMono:size=7,NotoColorEmoji:size=7,hack:size=8";
   };
-  services.wlsunset = {
-    enable = true;
-    latitude = lib.mkDefault "43.7159566";
-    longitude = lib.mkDefault "-79.3702805";
-    temperature = {
-      day = lib.mkDefault 5000;
-      night = lib.mkDefault 1500;
-    };
-  };
   systemd.user.services.swaybg = {
     Unit = {
       Description = "swaywm background";
@@ -143,12 +135,7 @@
   };
   home.packages = with pkgs;
     [
-      (pkgs.writeShellScriptBin "sunrise" ''
-        systemctl --user stop wlsunset
-      '')
-      (pkgs.writeShellScriptBin "sunset" ''
-        systemctl --user restart wlsunset
-      '')
+      nmap
 
       go-tools
       gotools
---
+++ nyiyui/wlsunset.nix
@@ -0,0 +1,20 @@
+{ pkgs, lib, ... }:
+{
+  services.wlsunset = {
+    enable = true;
+    latitude = lib.mkDefault "43.7159566";
+    longitude = lib.mkDefault "-79.3702805";
+    temperature = {
+      day = lib.mkDefault 5000;
+      night = lib.mkDefault 1500;
+    };
+  };
+  home.packages = [
+      (pkgs.writeShellScriptBin "sunrise" ''
+        systemctl --user stop wlsunset
+      '')
+      (pkgs.writeShellScriptBin "sunset" ''
+        systemctl --user restart wlsunset
+      '')
+    ];
+}
