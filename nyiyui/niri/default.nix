c01d141417555819256ae3181de8bd504d12510b
nyiyui <+@nyiyui.ca>
2024-04-11 02:32:55 -0400

    niri: remove taskbar

--- nyiyui/niri/default.nix
+++ nyiyui/niri/default.nix
@@ -1,13 +1,4 @@
 { config, pkgs, lib, specialArgs, ... }:
 {
   programs.niri.config = builtins.readFile ./config.kdl;
-
-  programs.waybar.settings.mainBar = {
-    modules-center = [ "wlr/taskbar" ];
-
-    "wlr/taskbar" = {
-      format = "{icon} {app_id} {title}";
-      icon = true;
-    };
-  };
 }
