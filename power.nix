678308d11035c9b4858d3be3c8694a1ff97c858c
nyiyui <+@nyiyui.ca>
2024-07-12 16:28:37 -0400

    power.nix: aggressive battery save (bc fan bad)

--- power.nix
+++ power.nix
@@ -14,7 +14,7 @@
 
     # 100 being the maximum, limit the speed of my CPU to reduce
     # heat and increase battery usage:
-    CPU_MAX_PERF_ON_AC = 70;
-    CPU_MAX_PERF_ON_BAT = 20;
+    CPU_MAX_PERF_ON_AC = 100;
+    CPU_MAX_PERF_ON_BAT = 10;
   };
 }
