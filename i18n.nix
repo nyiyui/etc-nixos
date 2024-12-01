323c21260d11728313ef59528e023a248be0ac3b
nyiyui <+@nyiyui.ca>
2024-12-01 15:11:49 -0500

    idk

--- i18n.nix
+++ i18n.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }: {
   i18n.defaultLocale = "ja_JP.UTF-8";
-  i18n.enabled = true;
-  i18n.type = "fcitx5";
   i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [ fcitx5-mozc fcitx5-hangul fcitx5-gtk ];
   };
 }
