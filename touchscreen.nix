6743e91a223066aab23178830d36ba59ab9c3deb
nyiyui <+@nyiyui.ca>
2025-01-18 16:37:26 -0500

    touchscreen: bugfix

--- touchscreen.nix
+++ touchscreen.nix
@@ -4,4 +4,5 @@
   home-manager.users.nyiyui = {
     imports = [ ./home-manager/touchscreen.nix ];
   };
+  users.users.nyiyui.extraGroups = [ "input" ]; # required for fusuma (see home-manager/touchscreen.nix)
 }
