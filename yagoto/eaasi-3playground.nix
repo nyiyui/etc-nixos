dfb776c208d038ee0a1a0d5aa1a4e561d34ed13c
nyiyui <+@nyiyui.ca>
2025-02-04 21:56:16 -0500

    oops

--- yagoto/eaasi-3playground.nix
+++ yagoto/eaasi-3playground.nix
@@ -1,10 +1,21 @@
 { config, ... }:
 {
+  age.secrets.eaasi-3playground-origincert = {
+    file = ../secrets/eaasi-3playground.nyiyui.ca.origincert.pem.age;
+    owner = "caddy";
+    mode = "400";
+  };
+  age.secrets.eaasi-3playground-privkey = {
+    file = ../secrets/eaasi-3playground.nyiyui.ca.privkey.pem.age;
+    owner = "caddy";
+    mode = "400";
+  };
   services.caddy = {
     enable = true;
-    virtualHosts."https://eaasi-playground.nyiyui.ca" = {
+    virtualHosts."https://eaasi-3playground.nyiyui.ca" = {
       extraConfig = ''
         reverse_proxy http://10.8.0.107:80
+        tls ${config.age.secrets.eaasi-3playground-origincert.path} ${config.age.secrets.eaasi-3playground-privkey.path}
       '';
     };
   };
