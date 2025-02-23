c38c8633923e0b3829b385efc99b02782296a15e
nyiyui <+@nyiyui.ca>
2025-02-22 22:32:56 -0500

    change audio by ±5%
    - useful on tablet where one which takes more time (side button)
    - I can't really tell the difference between 99% and 100%

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -109,8 +109,8 @@
           # use wev to find pressed keys
           "XF86AudioPlay" = "exec playerctl play-pause";
           "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-          "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +1%";
-          "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -1%";
+          "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
+          "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
           "Control+grave" = "exec playerctl play-pause";
           # Screenshots
           "Print" =
