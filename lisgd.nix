c0fd8e981b6da5afec53c5f9ad330ab8f3a29371
nyiyui <+@nyiyui.ca>
2025-03-02 21:23:16 -0500

    lisgd: add kill and wvkbd activation

--- lisgd.nix
+++ lisgd.nix
@@ -25,7 +25,9 @@ in {
           device=$(${pkgs.libinput}/bin/libinput list-devices | /run/current-system/sw/bin/grep -A 1 '${cfg.touchscreenDeviceName}' | /run/current-system/sw/bin/grep /dev/input | /run/current-system/sw/bin/awk '{print $2}')
           ${pkgs.lisgd}/bin/lisgd -d "$device" -v -m 800 \
             -g '1,LR,L,*,*,swaymsg workspace prev' \
-            -g '1,RL,R,*,*,swaymsg workspace next'
+            -g '1,RL,R,*,*,swaymsg workspace next' \
+            -g '1,UD,U,*,*,swaymsg kill' \
+            -g '1,DU,D,*,*,systemctl --user restart wvkbd.service'
         '';
         Install.WantedBy = [ "graphical-session.target" ];
       };
