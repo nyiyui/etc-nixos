f6d1dff14dff039625b0f914f70f0b80d73c782b
nyiyui <+@nyiyui.ca>
2025-03-17 15:28:57 -0400

    idk http no worky

--- yagoto/gtxr-vrsa.nix
+++ yagoto/gtxr-vrsa.nix
@@ -7,10 +7,21 @@ in {
     '';
     wantedBy = [ "multi-user.target" ];
   };
+  age.secrets.gtxr-vrsa-origincert = {
+    file = ../secrets/gtxr-vrsa.kiyuri.ca.origincert.pem.age;
+    owner = "caddy";
+    mode = "400";
+  };
+  age.secrets.gtxr-vrsa-privkey = {
+    file = ../secrets/gtxr-vrsa.kiyuri.ca.privkey.pem.age;
+    owner = "caddy";
+    mode = "400";
+  };
   services.caddy = {
     virtualHosts."gtxr-vrsa.kiyuri.ca" = {
       extraConfig = ''
         reverse_proxy localhost:${port}
+        tls ${config.age.secrets.gtxr-vrsa-origincert.path} ${config.age.secrets.gtxr-vrsa-privkey.path}
       '';
     };
   };
