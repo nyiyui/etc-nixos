18d167c0577c4b4b4a151bd4ce0ae530917a4e29
nyiyui <+@nyiyui.ca>
2025-03-22 11:11:55 -0400

    ye

--- yagoto/headscale.nix
+++ yagoto/headscale.nix
@@ -9,8 +9,8 @@ in
     settings = {
       server_url = "https://headscale.etc.kiyuri.ca";
       listen_addr = "localhost:${builtins.toString port}";
-      tls_cert_path = "";
-      tls_key_path = "";
+      tls_cert_path = null;
+      tls_key_path = null;
       # TODO: TS2021 Noise Protocol
       prefixes.v4 = "10.9.0.0/16";
       dns.base_domain = "tailnet.kiyuri.ca";
