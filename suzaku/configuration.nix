41abe2f71a32ca0924202fd61a4c5711f2f5bdfa
nyiyui <45807097+nyiyui@users.noreply.github.com>
2025-05-03 21:22:00 +0000

    Update configuration.nix

--- suzaku/configuration.nix
+++ suzaku/configuration.nix
@@ -120,4 +120,6 @@
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
 
+  networking.hostName = "suzaku";
+
 }
