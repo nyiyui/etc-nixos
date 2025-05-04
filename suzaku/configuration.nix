413ca71def531cf868cccedab8624d375fafcbad
Ken Shibata <ken.shibata@kiyuri.ca>
2025-05-04 20:58:43 +0000

    suzaku: shorter bootloader timeout

--- suzaku/configuration.nix
+++ suzaku/configuration.nix
@@ -26,6 +26,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 1;
 
   users.users.kiyurica = {
     initialHashedPassword =
