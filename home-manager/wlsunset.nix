5f6caa10a6fe4c503a32cd6a05ec8c0d3783222b
Ken Shibata <ken.shibata@kiyuri.ca>
2025-05-11 18:54:28 +0900

    wlsunset: switch to tokyo

--- home-manager/wlsunset.nix
+++ home-manager/wlsunset.nix
@@ -2,8 +2,10 @@
 {
   services.wlsunset = {
     enable = true;
-    latitude = lib.mkDefault "43.65";
-    longitude = lib.mkDefault "-79.38";
+    #latitude = lib.mkDefault "43.65";
+    #longitude = lib.mkDefault "-79.38"; # Toronto
+    latitude = lib.mkDefault "35.67";
+    longitude = lib.mkDefault "139.65"; # Tokyo
     temperature = {
       day = lib.mkDefault 6500;
       night = lib.mkDefault 2000;
