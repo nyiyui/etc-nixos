7c4f5b5696583280cafb112f355267a4f232d1f6
Ken Shibata <ken.shibata@kiyuri.ca>
2025-05-27 22:44:42 -0400

    remote-builder: update suzaku key
    I think impermanence wasn't caught up yet then

--- remote-builder.nix
+++ remote-builder.nix
@@ -11,7 +11,7 @@
         # NOTE: eva-00 and friends are not NixOS machines, and therefore have to have SSH keys synced to them by hand
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtA1YLDpuFOdXJRowvZEVx1X0M1YUDmo0Eaxjq5WSY2 root@misaki"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ9Z/KUpNuN0LYa3eczVJkMwjHULKbvu8Ii7P/BgPK52 root@mitsu8"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINO81KhmzXXt4N0XTRZomh9g64dHsq9sepbJcQzT63l8 root@suzaku"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDy5lcb0I4CjFMc9SCr2ZJYTqfnInabxDPS+AHcUSTiH root@suzaku"
       ];
     };
 
