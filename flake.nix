1b6cd79c2defbf9d959ee51e81710c742fc2f6b3
Ken Shibata <ken.shibata@kiyuri.ca>
2025-06-05 22:08:30 -0400

    Revert "bump to 25.05"
    This reverts commit 8dec7c20c2caf7f356b3eee7d1d3357365b71364.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     jks.url = "github:nyiyui/jks";
