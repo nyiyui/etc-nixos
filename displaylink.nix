8c0001c7bf22b2a0777f0b2e757dda08b5b26d42
Ken Shibata <ken.shibata@kiyuri.ca>
2025-06-25 22:51:48 -0400

    displaylink: run DisplayLinkManager as a service

--- displaylink.nix
+++ displaylink.nix
@@ -1,3 +1,5 @@
+# Support for DisplayLink devices.
+# Tested on Dell Universal Dock D6000 on 2025-Jun-25
 {
   config,
   lib,
@@ -15,5 +17,16 @@
     environment.systemPackages = with pkgs; [
       displaylink
     ];
+    # service based on https://aur.archlinux.org/displaylink.git commit f44fffc53789cc64c92f3eb0e22022f5daba65b3
+    systemd.services.displaylink-manager = {
+      after = [ "display-manager.service" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.displaylink}/bin/DisplayLinkManager";
+        WorkingDirectory = "${pkgs.displaylink}/lib/displaylink";
+        Restart = "always";
+        RestartSec = 5;
+      };
+      wantedBy = [ "graphical.target" ];
+    };
   };
 }
