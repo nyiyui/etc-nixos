12c985427f93654e9d7506ac1eb5ea1dc9490f1b
Ken Shibata <ken.shibata@kiyuri.ca>
2025-07-13 16:16:45 -0400

    joystick-as-mouse as home-manager command
    to be followed w/ better integration

---
+++ home-manager/joystick-as-mouse.nix
@@ -0,0 +1,16 @@
+{ pkgs, nixpkgs-unstable, ... }:
+let
+  pkgs-unstable = import nixpkgs-unstable { system = pkgs.system; };
+pythonEnv = pkgs-unstable.python3.withPackages (ps: with ps; [
+evdev
+python-uinput
+black
+]);
+in
+{
+  home.packages = [
+    (pkgs.writeShellScriptBin "joystick-as-mouse.sh" ''
+      ${pythonEnv}/bin/python3 ${./joystick_as_mouse.py}
+    '')
+  ];
+}
