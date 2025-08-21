ea4c9239992eba969c57ce85509b9cf76cd46e52
Ken Shibata <ken.shibata@kiyuri.ca>
2025-08-20 22:34:51 -0400

    inaho/minio: fix

--- inaho/minio.nix
+++ inaho/minio.nix
@@ -6,6 +6,26 @@
     listenAddress = "inaho.tailcbbed9.ts.net:9000";
     consoleAddress = "inaho.tailcbbed9.ts.net:9001";
     browser = true; # for testing
-    dataDir = [ "-S ${config.kiyurica.tailscale.cert.certPath} /var/lib/minio/data" ]; # workaround for nixos-24.11 (fixed in 25.05)
+    dataDir = [ "-S /var/lib/minio/certs /var/lib/minio/data" ]; # workaround for nixos-24.11 (fixed in 25.05)
+  };
+
+  systemd.services.minio-cert-copy = {
+    description = "Copy Tailscale certificates for MinIO";
+    wantedBy = [ "minio.service" ];
+    before = [ "minio.service" ];
+    after = [ "provision-tailscale-cert.service" ];
+    requires = [ "provision-tailscale-cert.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      mkdir -p /var/lib/minio/certs
+      cp ${config.kiyurica.tailscale.cert.certPath} /var/lib/minio/certs/public.crt
+      cp ${config.kiyurica.tailscale.cert.keyPath} /var/lib/minio/certs/private.key
+      chown -R minio:minio /var/lib/minio/certs
+      chmod 600 /var/lib/minio/certs/private.key
+      chmod 644 /var/lib/minio/certs/public.crt
+    '';
   };
 }
