ae0b734f243f82f470fddcbf00b5bf202d8c831f
Ken Shibata <ken.shibata@kiyuri.ca>
2025-09-09 11:32:32 -0400

    oops

--- home-manager/shpool.nix
+++ home-manager/shpool.nix
@@ -11,7 +11,7 @@
     # Auto-attach to shpool session
     if status is-interactive
       and not set -q SHPOOL_SESSION_NAME
-      shpool attach -d $(shuf -n 1 ${./shpool-names.txt}) || shpool daemon
+      shpool attach $(shuf -n 1 ${./shpool-names.txt}) || shpool daemon
       # TODO: select a non-preexsiting name
     end
   '';
