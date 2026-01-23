ce312b7e9d694813617dc36b2660dc398f9f0d05
Ken Shibata <ken.shibata@kiyuri.ca>
2026-01-23 02:43:00 -0500

    idk

--- laptop.nix
+++ laptop.nix
@@ -30,8 +30,8 @@
       powerManagement.powertop.enable = true;
       services.udev.extraRules = ''
         # Power adapter connected/disconnected detection
-        SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="${config.boot.kernelPackages.86_energy_perf_policy} power"
-        SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="${config.boot.kernelPackages.86_energy_perf_policy} performance"
+        SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="${config.boot.kernelPackages.x86_energy_perf_policy} power"
+        SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="${config.boot.kernelPackages.x86_energy_perf_policy} performance"
       '';
     };
 }
