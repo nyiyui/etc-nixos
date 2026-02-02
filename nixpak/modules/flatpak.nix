cc112a028f7c197bd98f046e3e677a6ef6c21662
Ken Shibata <ken.shibata@kiyuri.ca>
2026-02-01 19:54:56 -0500

    add docs

--- nixpak/modules/flatpak.nix
+++ nixpak/modules/flatpak.nix
@@ -1,4 +1,6 @@
-{ appId }: {
+# Use like so: imports = [ (import ../modules/flatpak.nix { appId = "com.example.app"; }) ]
+{ appId }:
+{
   config.flatpak.appId = appId;
   config.bubblewrap.env.FLATPAK_APP_ID = appId;
 }
