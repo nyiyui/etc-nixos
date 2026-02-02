47140e1edf5bbb36c1d5ff5afdb1d2b6f9e9a602
Ken Shibata <ken.shibata@kiyuri.ca>
2026-02-02 09:52:34 -0500

    try to make powertop parallel

--- laptop.nix
+++ laptop.nix
@@ -27,6 +27,7 @@
         };
       services.logind.settings.Login.HandleLidSwitchDocked = "ignore";
       networking.networkmanager.wifi.powersave = true;
+      systemd.services.powertop.after = lib.mkForce [ ]; # I don't think powertop needs to wait for everything in multi-user.target (esp. NetworkManager-wait-online.service).
       powerManagement.powertop.enable = true;
       services.udev.extraRules = ''
         # Power adapter connected/disconnected detection
