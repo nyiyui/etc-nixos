08e481b34cb96a889aef735a8effc659d211ff89
Ken Shibata <ken.shibata@kiyuri.ca>
2026-02-11 12:22:41 -0500

    oops

--- nixpak/modules/flatpak.nix
+++ nixpak/modules/flatpak.nix
@@ -6,5 +6,4 @@
   config.flatpak.appId = appId;
   config.bubblewrap.env.FLATPAK_ID = appId;
   config.bubblewrap.env.FLATPAK_APP_ID = appId; # TODO: leave it bc why not
-  config.dbus.policies."org.freedesktop.impl.portal.*" = "talk";
 }
--- nixpak/modules/xdg-portal.nix
+++ nixpak/modules/xdg-portal.nix
@@ -1,5 +1,6 @@
 { sloth, ... }:
 {
+  config.dbus.policies."org.freedesktop.impl.portal.*" = "talk";
   # NOTE: `GTK_USE_PORTAL=1` makes GTK route file picking through xdg-desktop-portal.
   config.bubblewrap.env.GTK_USE_PORTAL = "1";
   config.bubblewrap.bind.rw = [
