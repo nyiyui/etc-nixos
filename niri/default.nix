910b9b3b89df0857e8123d9fdd9f21309d8a4f92
Ken Shibata <ken.shibata@kiyuri.ca>
2026-02-14 19:43:39 -0500

    niri: cleanup env vars

--- niri/default.nix
+++ niri/default.nix
@@ -133,10 +133,6 @@
                     QT_QPA_PLATFORM = "wayland";
                     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
                     GDK_BACKEND = "wayland";
-                    GBM_BACKEND = "nvidia-drm";
-                    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-                    MOZ_ENABLE_WAYLAND = "1";
-                    WLR_NO_HARDWARE_CURSORS = "1";
                     INPUT_METHOD = "fcitx";
                     QT_IM_MODULE = "fcitx";
                     XMODIFIERS = "@im fcitx";
