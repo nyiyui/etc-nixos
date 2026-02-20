32e24579940792b0c55c37fe122352ec12b3b3ea
Ken Shibata <ken.shibata@kiyuri.ca>
2026-02-19 19:36:26 -0500

    oops

--- unity.nix
+++ unity.nix
@@ -7,7 +7,7 @@
 {
   options.assr.programs.unity.enable = lib.mkEnableOption "Unity 3D";
 
-  config = lib.mkIf config.assr.program.unity.enable {
+  config = lib.mkIf config.assr.programs.unity.enable {
     environment.systemPackages = [ pkgs.unityhub ];
   };
 }
