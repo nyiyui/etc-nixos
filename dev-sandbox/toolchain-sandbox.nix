4152b3caa6a4b013c0e5b5d646ce5388e35ed681
Ken Shibata <ken.shibata@kiyuri.ca>
2026-03-04 21:33:21 -0500

    oops fmt

--- dev-sandbox/toolchain-sandbox.nix
+++ dev-sandbox/toolchain-sandbox.nix
@@ -1,14 +1,21 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [ ../home-manager.nix ];
 
   config = lib.mkIf config.assr.dev-sandbox.enable {
     kiyurica.home-manager.enable = true;
-    home-manager.users.kiyurica = { ... }: {
-      programs.direnv = {
-        enable = true;
-        enableFishIntegration = true;
+    home-manager.users.kiyurica =
+      { ... }:
+      {
+        programs.direnv = {
+          enable = true;
+          enableFishIntegration = true;
+        };
       };
-    };
   };
 }
