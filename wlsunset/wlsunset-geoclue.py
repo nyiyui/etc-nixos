2d9d23a220faa770c3311f3bb61ff85d509c136f
Ken Shibata <ken.shibata@kiyuri.ca>
2026-03-23 00:06:39 +0900

    wlsunset-geoclue: loosen rounding
    geoclue was jumping around

--- wlsunset/wlsunset-geoclue.py
+++ wlsunset/wlsunset-geoclue.py
@@ -20,8 +20,8 @@ extra_args = sys.argv[1:]
 def close_enough(a, b):
     # Generally, 3 digits for lat/lon is supposedly an accuracy of ~100 metres,
     # which is more than enough for solar calculations.
-    return round(a[0], 3) == round(b[0], 3) and round(a[1], 3) == round(
-        b[1], 3
+    return round(a[0], 0) == round(b[0], 0) and round(a[1], 1) == round(
+        b[1], 1
     )
 
 
