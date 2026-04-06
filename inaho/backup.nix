b9c6bee90712e64231c2a35a7434d1f1edc44258
Ken Shibata <ken.shibata@kiyuri.ca>
2026-04-06 10:45:17 -0400

    yes

--- inaho/backup.nix
+++ inaho/backup.nix
@@ -55,7 +55,10 @@
     };
   };
   systemd.services.backup-vps-7de6b7ba = {
-    path = [ pkgs.rsync pkgs.openssh ];
+    path = [
+      pkgs.rsync
+      pkgs.openssh
+    ];
     script = ''
       rsync -avzc -e "ssh -o StrictHostKeyChecking=no -i $CREDENTIALS_DIRECTORY/backup-vps-7de6b7ba" backup-access@vps-7de6b7ba.tailcbbed9.ts.net:/var/lib/convind4 /backups/vps-7de6b7ba
     '';
