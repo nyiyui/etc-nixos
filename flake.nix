dcc96a026a07957787ed974a92980d6dd1350a9d
Ken Shibata <ken.shibata@kiyuri.ca>
2026-04-07 23:31:39 -0400

    actually include patched nix?
    From https://discourse.nixos.org/t/nix-security-advisory-privilege-escalation-via-symlink-following-during-fod-output-registration/76900/12:
    
    Just as a note, if your flake or channel uses nixos-25.11 or even nixos-unstable at the time of writing the patches have not been ported to these branches/streams.
    
    Instead, for 25.11 use release-25.11.
    
    nix-shell -p nix -I nixpkgs=channel:nixos-25.11 --run "nix --version"
    
    nix (Nix) 2.31.3
     > nix-shell -p nix -I nixpkgs=channel:nixos-unstable --run "nix --version"
    
    nix (Nix) 2.31.3+1
    
    Update your system configuration flake to point to the correct ref that includes the patch for nixpkgs:
    
    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11"; # temporary for CVE-2026-39860
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
