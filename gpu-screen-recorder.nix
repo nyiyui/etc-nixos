42f1a5e3b4ef25eb6f89a95a876096d1d4977e59
Ken Shibata <ken.shibata@kiyuri.ca>
2026-04-30 16:54:14 -0400

    gpu-screen-recorder w/ portal no worky

--- gpu-screen-recorder.nix
+++ gpu-screen-recorder.nix
@@ -18,21 +18,6 @@ in
     environment.systemPackages = with pkgs; [
       gpu-screen-recorder
       gpu-screen-recorder-gtk
-
-      # Synthesize a .desktop file so GNOME's portal trusts the binary
-      # (according to Gemini, untested)
-      (pkgs.makeDesktopItem {
-        name = "gpu-screen-recorder";
-        desktopName = "GPU Screen Recorder CLI";
-        exec = "gpu-screen-recorder";
-        icon = "video-display";
-        terminal = true;
-        type = "Application";
-        categories = [
-          "AudioVideo"
-          "Recorder"
-        ];
-      })
     ];
   };
 }
