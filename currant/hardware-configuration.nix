cffe0c2a2c46eaa37246f78bbd2f3aa110dbc566
Ken Shibata <ken.shibata@kiyuri.ca>
2026-05-01 16:47:50 -0400

    oops

--- currant/hardware-configuration.nix
+++ currant/hardware-configuration.nix
@@ -13,11 +13,6 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-      fsType = "ext4";
-    };
-
   swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
