aeceddd3ed3b35e67e6080203bf3d51aa5de1d76
Ken Shibata <ken.shibata@kiyuri.ca>
2026-05-14 23:34:47 +0900

    oops

--- appliance/deploy-local.sh
+++ appliance/deploy-local.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env dash
 
-nix build .#nixosConfigurations.suzaku.config.system.build.sysupdate-package
-local UKI_UNSIGNED="result/assr-appliance-uki_*.efi"
-local UKI_SIGNED="$(basename "$UKI_UNSIGNED")"
+set -eux
+
+nix build -L .#nixosConfigurations.suzaku.config.system.build.sysupdate-package
+UKI_UNSIGNED="result/assr-appliance-uki_*.efi"
+UKI_SIGNED="$(basename "$UKI_UNSIGNED")"
 sbctl sign "$UKI_UNSIGNED" -o "$UKI_SIGNED"
 # NOTE: for server uploads, SHA256SUMS must be updated to match signed UKI
 run0 install -D -o root -g root result/assr-appliance_*.root.raw /var/lib/updates/
