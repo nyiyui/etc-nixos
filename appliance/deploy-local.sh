65274513c22898da7328bf99c2736675d2aeeb00
Ken Shibata <ken.shibata@kiyuri.ca>
2026-05-15 15:39:37 +0900

    oooooops

--- appliance/deploy-local.sh
+++ appliance/deploy-local.sh
@@ -7,6 +7,6 @@ UKI_UNSIGNED="$(echo result/assr-appliance-uki_*.efi)"
 UKI_SIGNED="$(mktemp -d)/$(basename "$UKI_UNSIGNED")"
 run0 sbctl sign "$UKI_UNSIGNED" -o "$UKI_SIGNED"
 # NOTE: for server uploads, SHA256SUMS must be updated to match signed UKI
-run0 install -D -o root -g root result/assr-appliance_*.root.raw /var/lib/updates/
-run0 install -D -o root -g root result/assr-appliance_*.root-verity.raw /var/lib/updates/
+run0 install -D -o root -g root result/assr-appliance_*.root.*.raw /var/lib/updates/
+run0 install -D -o root -g root result/assr-appliance_*.root-verity.*.raw /var/lib/updates/
 run0 install -D -o root -g root $UKI_SIGNED /var/lib/updates/
