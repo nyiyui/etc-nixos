d75e66e0ba7395907c3b6d772e9318dd3e611279
Ken Shibata <ken.shibata@kiyuri.ca>
2026-05-16 19:45:45 +0900

    idk

--- appliance/ephemeral-sysroot.nix
+++ appliance/ephemeral-sysroot.nix
@@ -7,6 +7,24 @@
 let
   mapper = "crypt-ephemeral-sysroot";
   partlabel = "ephemeral-sysroot";
+  script = pkgs.writeShellScript "ephemeral-sysroot.sh" ''
+    echo "Generating ephemeral encryption key..."
+    dd if=/dev/urandom of=/tmp/ephemeral.key bs=512 count=1 status=none
+
+    echo "Formatting LUKS container..."
+    cryptsetup luksFormat --type luks2 \
+      --pbkdf pbkdf2 --pbkdf-force-iterations 1000 \
+      --batch-mode --key-file /tmp/ephemeral.key "$PART"
+
+    echo "Opening LUKS container..."
+    cryptsetup open --key-file /tmp/ephemeral.key "$PART" ${lib.strings.escapeShellArg mapper}
+
+    echo "Formatting inner volume as ext4..."
+    mkfs.ext4 -F -q /dev/mapper/${lib.strings.escapeShellArg mapper}
+
+    # Securely wipe the ephemeral key from the initrd RAM
+    rm /tmp/ephemeral.key
+  '';
 in
 {
   options.assr.appliance.ephemeral-sysroot-size = lib.mkOption {
@@ -23,34 +41,19 @@ in
       SizeMaxBytes = config.assr.appliance.ephemeral-sysroot-size;
     };
   };
-  config.boot.initrd.systemd.storePaths = with pkgs; [ cryptsetup e2fsprogs coreutils ];
+  config.boot.initrd.systemd.storePaths = with pkgs; [ cryptsetup e2fsprogs coreutils script ];
   config.boot.initrd.systemd.services.ephemeral-sysroot = {
     description = "format ephemeral sysroot for this boot";
     wantedBy = [ "initrd.target" ];
     before = [ "sysroot.mount" ];
-    serviceConfig.Type = "oneshot";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${script}";
+    };
     bindsTo = [ "dev-disk-by\\x2dpartlabel-ephemeral\\x2dsysroot.device" ];
     after   = [ "dev-disk-by\\x2dpartlabel-ephemeral\\x2dsysroot.device" ];
     environment.PART = "/dev/disk/by-partlabel/${partlabel}";
     enableStrictShellChecks = true;
-    script = ''
-      echo "Generating ephemeral encryption key..."
-      dd if=/dev/urandom of=/tmp/ephemeral.key bs=512 count=1 status=none
-
-      echo "Formatting LUKS container..."
-      cryptsetup luksFormat --type luks2 \
-        --pbkdf pbkdf2 --pbkdf-force-iterations 1000 \
-        --batch-mode --key-file /tmp/ephemeral.key "$PART"
-
-      echo "Opening LUKS container..."
-      cryptsetup open --key-file /tmp/ephemeral.key "$PART" ${lib.strings.escapeShellArg mapper}
-
-      echo "Formatting inner volume as ext4..."
-      mkfs.ext4 -F -q /dev/mapper/${lib.strings.escapeShellArg mapper}
-
-      # Securely wipe the ephemeral key from the initrd RAM
-      rm /tmp/ephemeral.key
-    '';
   };
 
   config.fileSystems."/" = {
