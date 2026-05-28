b456f363b0cfc9e0a68d5243c8a7930add2c0cb1
Ken Shibata <ken.shibata@kiyuri.ca>
2026-05-28 14:09:13 +0900

    doas.nix: disable sudo (that's the point of doas.nix)

--- doas.nix
+++ doas.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 {
   # Priviledge Escalation
-  security.sudo.enable = lib.mkDefault false;
+  security.sudo.enable = false;
   security.doas.enable = true;
   security.doas.extraRules = [
     {
