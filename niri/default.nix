7718e09380ea24455942d5af98ecde50eac17bb7
Ken Shibata <ken.shibata@kiyuri.ca>
2026-08-16 22:51:11 +0900

    use nixpkgs default config now

--- niri/default.nix
+++ niri/default.nix
@@ -367,15 +367,10 @@
     xdg.portal = {
       enable = true;
       # https://github.com/YaLTeR/niri/wiki/Screencasting
-      # Use GTK portal for the file picker; keep GNOME portal available for screencasting.
       extraPortals = with pkgs; [
         xdg-desktop-portal-gtk
         xdg-desktop-portal-gnome
       ];
-      config.niri = {
-        default = "gtk";
-        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
-      };
       xdgOpenUsePortal = true;
     };
 
