72d5830493b9d0c64a9deb8346bd0f2a968960a2
Ken Shibata <ken.shibata@kiyuri.ca>
2026-08-31 17:13:22 -0400

    gatech-vpn: regenerate password cred file

--- gatech-vpn.nix
+++ gatech-vpn.nix
@@ -19,17 +19,12 @@
     };
 
   config = lib.mkIf config.kiyurica.gatech-vpn.enable {
-    age.secrets."gatech-vpn-password.cred" = {
-      file = ./secrets/gatech-vpn-password-${config.networking.hostName}.cred.age;
-      owner = config.kiyurica.ocproxy.user;
-      mode = "400";
-    };
     kiyurica.ocproxy = {
       enable = true;
       server = "vpn.gatech.edu";
       gateway = "DC Gateway";
       username = "kshibata6";
-      password-file = config.age.secrets."gatech-vpn-password.cred".path;
+      password-file = ./secrets/gatech-vpn-password-${config.networking.hostName}.cred;
     };
 
     home-manager.users.kiyurica = lib.mkIf config.kiyurica.home-manager.enable {
