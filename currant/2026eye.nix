41f0a83cd450e659319bc82584ea540d49845892
Ken Shibata <ken.shibata@kiyuri.ca>
2026-09-06 20:42:47 -0400

    nixfmt

--- currant/2026eye.nix
+++ currant/2026eye.nix
@@ -3,7 +3,7 @@
     isNormalUser = true;
     description = "2026eye sftp";
     createHome = true;
-  
+
     openssh.authorizedKeys.keys = [
       ''from="192.168.2.118",command="internal-sftp",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUwsMg/p63AeCWPvphJ7SwW4tLYVwMS2AApAf1+LkOF kiyurica@2026eye''
     ];
