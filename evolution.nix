f41a1f380a3dd6952a98f2db0c83fdd4e5d8db9f
Ken Shibata <ken.shibata@kiyuri.ca>
2026-09-10 21:55:57 -0400

    fix evolution.nix

--- evolution.nix
+++ evolution.nix
@@ -3,5 +3,5 @@
   programs.evolution.enable = true;
   programs.evolution.plugins = [ pkgs.evolution-ews ];
   services.gnome.evolution-data-server.enable = true;
-  services.gnome-keyring.enable = true;
+  services.gnome.gnome-keyring.enable = true;
 }
