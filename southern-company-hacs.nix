b75c79b2edcec47e1d4750bc90d6d5dba0b9629d
Ken Shibata <ken.shibata@kiyuri.ca>
2026-09-15 22:26:19 -0400

    currant: update southern co to git main
    1.0.0 is broken

--- southern-company-hacs.nix
+++ southern-company-hacs.nix
@@ -9,13 +9,13 @@
 let
   southern-company-api = python314Packages.buildPythonPackage rec {
     pname = "southern-company-api";
-    version = "0.6.5";
+    version = "0.7.1";
     pyproject = true;
 
     src = fetchPypi {
       inherit version;
       pname = "southern_company_api";
-      hash = "sha256-vOcU2Pbi9mdiggFT/QIlNeX0prsJUg65phOnTKOwJBU=";
+      hash = "sha256-Bfe2xpP4AFsBTnk1TsCBciFf7uZPKNoSU0r74b5QWHU=";
     };
 
     build-system = [ python314Packages.poetry-core ];
@@ -29,13 +29,15 @@ in
 buildHomeAssistantComponent rec {
   owner = "Southern-Company-HA";
   domain = "southern_company";
-  version = "1.0.0";
+  # Tracking main instead of the 1.0.0 tag: main has fixes not yet released,
+  # e.g. the Georgia Power auth chain/reauth fix (#122).
+  version = "unstable-2026-09-10";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "southern-company-hacs";
-    tag = version;
-    hash = "sha256-xh7I+5nu/pg1IfTPe/dj+zCDmPnhmpiE5qY4vgiP+0U=";
+    rev = "2da7f8dffe77ee37d9046b66a74f9c2ac077df26";
+    hash = "sha256-CiHt+A7RvAw5w89VrfZtLC/fArgfvjMAtRfzb+bezBI=";
   };
 
   propagatedBuildInputs = [ southern-company-api ];
