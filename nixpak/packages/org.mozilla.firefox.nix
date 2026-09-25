ae62aaf445b2fa8fdd9680d58d5c23dfb8dd0743
Ken Shibata <ken.shibata@kiyuri.ca>
2026-09-24 20:33:50 -0400

    suzaku: support usb webcam

--- nixpak/packages/org.mozilla.firefox.nix
+++ nixpak/packages/org.mozilla.firefox.nix
@@ -49,7 +49,9 @@ let
           bind.dev = [
             "/dev/shm"
           ]
-          ++ map (i: "/dev/hidraw${toString i}") (lib.range 0 15);
+          ++ map (i: "/dev/hidraw${toString i}") (lib.range 0 15)
+          # needed for USB webcams (uvcvideo)
+          ++ map (i: "/dev/video${toString i}") (lib.range 0 15);
         };
       };
   };
